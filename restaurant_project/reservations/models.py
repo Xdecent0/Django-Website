76978918675b2479deeae924c960ff0b235a126c
Xdecent <xdecentwork@gmail.com>
2023-12-18 15:41:57 +0200

    created models

--- restaurant_project/reservations/models.py
+++ restaurant_project/reservations/models.py
@@ -1,3 +1,19 @@
+# reservations/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# reservations/models.py
+class Table(models.Model):
+    number = models.IntegerField(unique=True)
+    seats = models.IntegerField()
+    shape = models.CharField(max_length=20, choices=[('rectangle', 'Прямоугольный'), ('oval', 'Овальный')])
+    coordinates_horizontal = models.FloatField()
+    coordinates_vertical = models.FloatField()
+    width_percentage = models.FloatField()
+    length_percentage = models.FloatField()
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+
